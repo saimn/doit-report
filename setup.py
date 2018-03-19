fe5866bdedf1b16a09aabc2e6ab556f23f105d18
Simon Conseil <contact@saimon.org>
2018-03-19 10:52:02 +0100

    Include LICENSE and add author_email

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     license='MIT',
     author='Simon Conseil',
+    author_email='contact@saimon.org',
     url='http://github.com/saimn/doit-report',
     description='Show ASCII or HTML report for doit task execution status',
     long_description=open('README.md').read(),
